50c88e9b74f83fc06229302e0314748c2253c2e4
Mango <mango@Mangos-MacBook-Pro.local>
2014-02-10 14:32:48 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '669b5904bb30396909f876c01f5785d18d9ea836aaba96ac329f146fde1b47adb5ae8fd1185524fd51273c7b9a3a53a03c0f77e5577f28f9dc73f50f421b553d'
